f96998bdb91605c9504457e5731cffccc97e153f
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-08-01 12:35:51 -0300

    Contexto de datos

---
+++ Productos.Server/Models/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Productos.Server.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage ="El campo {0} debe tener máximo {1} caratéres")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [DataType(DataType.MultilineText)]
+        [MaxLength(500, ErrorMessage ="El campo {0} debe tener máximo {1} caractéres")]
+        public string Descripcion { get; set; } = string.Empty;
+
+        [Column(TypeName ="decimal(18,2)")]
+        [DisplayFormat(DataFormatString ="{0:C2}")] //Formato moneda
+        public decimal Precio { get; set; }
+    }
+}
---
+++ Productos.Server/Models/ProductosContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Drawing;
+
+namespace Productos.Server.Models
+{
+    public class ProductosContext : DbContext
+    {
+        public ProductosContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+
+        //este código asegura que cualquier configuración básica del modelo de datos
+        //definida en la clase base (DbContext) se mantenga y permite agregar configuraciones
+        //adicionales específicas del modelo a través del parámetro modelBuilder.
+        //Si planeas agregar configuraciones personalizadas como definir relaciones entre
+        //entidades, configuraciones o restricciones, entonces sí es útil para incluir esas configuraciones.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Producto>().HasIndex(p => p.Nombre).IsUnique();
+            //Evitamos duplicados de nombre:
+            //.HasIndex(p => p.Nombre): Indica que se debe crear un índice en la columna Nombre de la tabla Producto.
+            //.IsUnique(): Especifica que el índice debe ser único, lo que significa que no puede haber dos productos con el mismo valor en la columna Nombre.
+        }
+        
+    }
+}
